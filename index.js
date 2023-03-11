ba53d6908d44b8ccefdd5f28666cc175cd338db3
aseelflihan <aseelflihan@gmail.com>
2023-03-11 15:00:20 +0300

    complate assignment

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const reversedWord = word.split("").reverse().join("");
+  return word === reversedWord;
 }
 
 /* 
   Add your pseudocode here
+
+
+  Pseudocode:
+  1. Define a function called isPalindrome that takes in a word parameter.
+  2. Create a variable called reversedWord and set it equal to the reverse of the word parameter.
+  3. If the word parameter is equal to the reversedWord, return true.
+  4. Otherwise, return false.
 */
+
+
 
 /*
   Add written explanation of your solution here
+
+   Written explanation:
+  The isPalindrome function takes in a word parameter and checks if it's a palindrome.
+  To check if a word is a palindrome, we reverse the order of its characters and then compare it with the original word.
+  If the two are the same, then the word is a palindrome.
+  
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
   console.log("=>", isPalindrome("racecar"));
+  console.log("=>", isPalindrome("a"));
 
   console.log("");
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+  console.log("=>", isPalindrome("ab"));
 }
 
 module.exports = isPalindrome;
